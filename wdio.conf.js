65850272fb2c73e7926479c034f523e9c6a7b4ef
Cindy Marcela Restrepo Vargas <cindymrv@gmail.com>
2024-10-23 18:11:02 -0500

    ✨ Agregar configuracion

---
+++ wdio.conf.js
@@ -0,0 +1,78 @@
+export const config = {
+    runner: 'local',
+    specs: [
+        './features/**/*.feature'
+    ],
+    exclude: [],
+    maxInstances: 10,
+    capabilities: [{
+        maxInstances: 5,
+        browserName: 'chrome',
+        acceptInsecureCerts: true,
+        'goog:chromeOptions': {
+            args: ['--window-size=1920,1080']
+        }
+    }],
+    logLevel: 'info',
+    bail: 0,
+    baseUrl: 'https://www.saucedemo.com',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    framework: 'cucumber',
+    reporters: [
+        'spec',
+        ['allure', {
+            outputDir: 'allure-results',
+            disableWebdriverStepsReporting: false,
+            disableWebdriverScreenshotsReporting: false,
+            useCucumberStepReporter: true
+        }]
+    ],
+    cucumberOpts: {
+        // Archivos donde están los step definitions
+        require: ['./features/step-definitions/*.js'],
+        backtrace: false,
+        requireModule: [],
+        dryRun: false,
+        failFast: false,
+        snippets: true,
+        source: true,
+        strict: false,
+        tagExpression: '',
+        timeout: 60000,
+        ignoreUndefinedDefinitions: false
+    },
+
+    //
+    // Hooks
+    before: function (capabilities, specs) {
+        browser.maximizeWindow();
+    },
+    afterTest: async function(test, context, { error, result, duration, passed, retries }) {
+        if (error) {
+            await browser.takeScreenshot();
+        }
+    },
+    onComplete: function() {
+        // Generar el reporte de Allure al finalizar las pruebas
+        const reportError = new Error('Could not generate Allure report')
+        const generation = allure(['generate', 'allure-results', '--clean'])
+        return new Promise((resolve, reject) => {
+            const generationTimeout = setTimeout(
+                () => reject(reportError),
+                5000)
+
+            generation.on('exit', function(exitCode) {
+                clearTimeout(generationTimeout)
+
+                if (exitCode !== 0) {
+                    return reject(reportError)
+                }
+
+                console.log('Allure report successfully generated')
+                resolve()
+            })
+        })
+    }
+}
