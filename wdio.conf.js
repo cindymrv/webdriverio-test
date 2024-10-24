8551d656bd38838d116dd2124ed78b4e73e0d86f
Cindy Marcela Restrepo Vargas <cindymrv@gmail.com>
2024-10-24 15:51:06 -0500

    ✨ Agregar configuracion allure

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,5 @@
+const allure = require('allure-commandline');
+
 exports.config = {
     specs: [
         './test/specs/**/*.js'
@@ -20,43 +22,23 @@ exports.config = {
         ['allure', {
             outputDir: 'allure-results',
             disableWebdriverStepsReporting: false,
-            disableWebdriverScreenshotsReporting: false
+            disableWebdriverScreenshotsReporting: false,
+            addConsoleLogs: true
         }]
     ],
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000
     },
-
     //
     // Hooks
     before: function (capabilities, specs) {
         browser.maximizeWindow();
     },
     afterTest: async function(test, context, { error, result, duration, passed, retries }) {
-        if (error) {
-            await browser.takeScreenshot();
+        if (!passed) {
+            const screenshot = await browser.takeScreenshot();
+            await allure.attachment('Screenshot', Buffer.from(screenshot, 'base64'), 'image/png');
         }
     },
-    onComplete: function() {
-        // Generar el reporte de Allure al finalizar las pruebas
-        const reportError = new Error('Could not generate Allure report')
-        const generation = allure(['generate', 'allure-results', '--clean'])
-        return new Promise((resolve, reject) => {
-            const generationTimeout = setTimeout(
-                () => reject(reportError),
-                5000)
-
-            generation.on('exit', function(exitCode) {
-                clearTimeout(generationTimeout)
-
-                if (exitCode !== 0) {
-                    return reject(reportError)
-                }
-
-                console.log('Allure report successfully generated')
-                resolve()
-            })
-        })
-    }
 }
